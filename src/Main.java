bd60f55a9775bc350b772e1ba938fa2b25b9007a
John Nolcox <jnolcox0429+github@gmail.com>
2018-01-21 12:54:02 -0500

    loop to continue the program until exit

--- src/Main.java
+++ src/Main.java
@@ -10,56 +10,62 @@ public class Main {
 
         //TODO figure out how to make this loop until the user decides to exit the program.
 
-        System.out.println("Enter Selection: \n");
-        System.out.println(
-                "1 - Addition\n" +
-                        "2 - Subtraction\n" +
-                        "3 - Multiplication\n" +
-                        "4 - Division\n" +
-                        "5 - Exit Program\n"
-        );
+        int selection;
 
-        Scanner kb = new Scanner(System.in);
-        int selection = kb.nextInt();
+        do {                                                //Tell the compiler to do the following code first, "Hey computer, do this for me"
+            System.out.println("Enter Selection: \n");
+            System.out.println(
+                            "1 - Addition\n" +
+                            "2 - Subtraction\n" +
+                            "3 - Multiplication\n" +
+                            "4 - Division\n" +
+                            "5 - Exit Program\n"
+            );
 
-        switch (selection) {
-            case 1: {
-                System.out.println("Please enter two numbers to be added: ");
-                int one = kb.nextInt();
-                int two = kb.nextInt();
-                Calculator.addition(one, two);
-                break;
+            Scanner kb = new Scanner(System.in);
+            selection = kb.nextInt();
+
+            switch (selection) {
+                case 1: {
+                    System.out.println("Please enter two numbers to be added: ");
+                    int one = kb.nextInt();
+                    int two = kb.nextInt();
+                    Calculator.addition(one, two);
+                    break;
+                }
+
+                case 2: {
+                    System.out.println("Please enter two numbers to be subtract: ");
+                    int one = kb.nextInt();
+                    int two = kb.nextInt();
+                    Calculator.subtraction(one, two);
+                    break;
+                }
+
+                case 3: {
+                    System.out.println("Please enter two numbers to be multiplied: ");
+                    int one = kb.nextInt();
+                    int two = kb.nextInt();
+                    Calculator.multiply(one, two);
+                    break;
+                }
+
+                case 4: {
+                    System.out.println("Please enter two numbers to be divded: ");
+                    double one = kb.nextDouble();
+                    double two = kb.nextDouble();
+                    Calculator.divide(one, two);
+                    break;
+                }
+
+                case 5: break;
+
+                default: break;
+
             }
-
-            case 2: {
-                System.out.println("Please enter two numbers to be subtract: ");
-                int one = kb.nextInt();
-                int two = kb.nextInt();
-                Calculator.subtraction(one, two);
-                break;
-            }
-
-            case 3: {
-                System.out.println("Please enter two numbers to be multiplied: ");
-                int one = kb.nextInt();
-                int two = kb.nextInt();
-                Calculator.multiply(one, two);
-                break;
-            }
-
-            case 4: {
-                System.out.println("Please enter two numbers to be divded: ");
-                double one = kb.nextDouble();
-                double two = kb.nextDouble();
-                Calculator.divide(one, two);
-                break;
-            }
-
-            case 5: break;
-
-            default: break;
-
-        }
+        } while (selection!=5); //This tell the compiler to check after running the above code, if the selection is 5
+        //If the selection is 5, then the program will exit, if it is not 5, it will loop back up the beginning of the "Do" block
+        //"Hey computer, check to see if they entered '5', if they didn't, keep going until the do"
 
         System.exit(0);
 
